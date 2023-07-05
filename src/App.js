ab7af58566e297c803cce9b32e70ea41595fcef7
Kushal Tiwari <tiwarikushal057@gmail.com>
2023-07-05 12:31:16 +0545

    fixed toggleImportanceOf function

--- src/App.js
+++ src/App.js
@@ -37,26 +37,9 @@ const App = () => {
     setNewNote(event.target.value)
   }
 
-  const toggleImportanceOf = (id) => {
-    const note = notes.find(n => n.id === id)
-    const changedNote = { ...note, important: !note.important }
-  
-    noteService
-      .update(id, changedNote)
-      .then(returnedNote => {
-        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
-      })
-      .catch(error => {
-        alert(
-          `the note '${note.content}' was already deleted from server`
-        )
-        setNotes(notes.filter(n => n.id !== id))
-      })
-  }
-
   const notesToShow = showAll
     ? notes
-    : notes.filter(note => note.important)
+    : notes.filter(note => note.important);
 
    const toggleImportanceOf = id => {
       const note = notes.find(n => n.id === id)
