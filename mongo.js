88cdbaf6f52b463283e1d0b85e95dfbd4569f9ee
Kushal Tiwari <tiwarikushal057@gmail.com>
2023-07-12 19:23:32 +0545

    connected to mongoDB atlas and pushed some notes into the database

--- mongo.js
+++ mongo.js
@@ -1,32 +1,30 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 if (process.argv.length<3) {
-    console.log('give password as argument')
-    process.exit(1)
+  console.log('give password as argument')
+  process.exit(1)
 }
 
-const password = process.argv[2];
+const password = process.argv[2]
 
-const url = "mongodb+srv://tiwarikushal057:<password>@cluster1.lz8ykwn.mongodb.net/?retryWrites=true&w=majority"
+const url = `mongodb+srv://zenithempres:${password}@cluster0.zald5xu.mongodb.net/noteApp?retryWrites=true&w=majority`
 
-mongoose.set('strictQuery', false);
-mongoose.connect(url);
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
 
-const noteSchema = new mongoose.Schema(
-    {
-        content: String,
-        important: Boolean
-    }
-)
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
 
-const Note = mongoose.model('Note', noteSchema);
+const Note = mongoose.model('Note', noteSchema)
 
 const note = new Note({
-    content: 'HTML is Easy',
-    important: true,
+  content: 'Life is hard but react is harder',
+  important: true,
 })
 
 note.save().then(result => {
-    console.log('note saved');
-    mongoose.connection.close();
+  console.log("note saved!")
+  mongoose.connection.close()
 })
